70e37c873bfa35d89d23d028c522f02109f0c402
Corentin Henry <corentinhenry@gmail.com>
2019-06-26 12:04:08 +0200

    add message form

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,14 +1,36 @@
 (ns guestbook.core
   (:require [reagent.core :as r]))
 
+(defn message-form []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input
+         {:type :text
+          :name :name
+          :on-change #(swap! fields assoc :name (-> % .-target .-value))
+          :value (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [:textarea.textarea
+         {:name :message
+          :value (:message @fields)
+          :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit
+         :value "comment"}]
+       ;; We can see the atom being updated when we type in one of the fields:
+       ;; [:p "Name: " (:name @fields)]
+       ;; [:p "Message: " (:message @fields)]
+       ])))
+
+(defn home []
+  [:div.content>div.columns.is-centered>div.column.is-two-thirds
+   [:div.columns>div.column
+    [message-form]]])
+
 (r/render
- ;; equivalent to (> for nesting the <h1> tag)
- ;;     [:div#hello.content [:h1 "Hello, Auto!"]]
- ;;
- ;; which is equivalent to (# for the id, . for the class)
- ;;     [:div {:id "hello", :class "content"} [:h1 "Hello, Auto!"]]
- ;;
- ;; wich is turned into:
- ;;     <div id="hello" class="content"><h1>Hello, Auto!</h1></div>
- [:div#hello.content>h1 "Hello, Auto!"]
+ (home)
  (.getElementById js/document "content"))
