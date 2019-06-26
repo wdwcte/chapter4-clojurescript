d734fb21d26b9ca43ded1a4068ba08ebaeff9a31
Corentin Henry <corentinhenry@gmail.com>
2019-06-26 11:43:46 +0200

    more involved reagent "Hello World"

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -2,5 +2,13 @@
   (:require [reagent.core :as r]))
 
 (r/render
- [:h1 "Hello, Reagent"]
+ ;; equivalent to (> for nesting the <h1> tag)
+ ;;     [:div#hello.content [:h1 "Hello, Auto!"]]
+ ;;
+ ;; which is equivalent to (# for the id, . for the class)
+ ;;     [:div {:id "hello", :class "content"} [:h1 "Hello, Auto!"]]
+ ;;
+ ;; wich is turned into:
+ ;;     <div id="hello" class="content"><h1>Hello, Auto!</h1></div>
+ [:div#hello.content>h1 "Hello, Auto!"]
  (.getElementById js/document "content"))
